956659a2d2554bd61a804cb8bcb9160252580f4f
myhan601 <59227948+myhan601@users.noreply.github.com>
2024-03-01 22:38:58 +0900

    first commit

---
+++ PracticeUI/MyTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MyTableViewController.swift
+//  PracticeUI
+//
+//  Created by 한철희 on 2/27/24.
+//
+
+import UIKit
+
+class MyTableViewController: UIViewController {
+    @IBOutlet weak var myTableView: UITableView!
+    
+    let friendNames: [String] = ["Henry", "Leeo", "Jay", "Key"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        myTableView.backgroundColor = .blue
+        myTableView.delegate = self
+        myTableView.dataSource = self
+    }
+    
+}
+
+extension MyTableViewController:UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friendNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = myTableView.dequeueReusableCell(withIdentifier: "MyFirstCell", for: indexPath)
+        cell.textLabel?.text = friendNames[indexPath.row]
+        return cell
+    }
+}
